232904f20a2c1f49c374dbad85853ac4c49721b6
Zaid658 <zaidabduljabbar420@gmail.com>
2024-10-29 22:46:26 +0500

    Final Project Completed

---
+++ home.js
@@ -0,0 +1,10 @@
+
+<script>
+function out() {
+document.getElementById("ezk e-p/cards/card (1).jpg").src = "imgs/off.png";
+}
+
+function over() {
+document.getElementById("ezk e-p/cards/trophy.jpg").src = "imgs/on.png";
+}
+</script>
